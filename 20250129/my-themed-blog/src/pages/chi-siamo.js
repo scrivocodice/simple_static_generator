ac310a61cbba11c479b109bc60aa3d49c699245d
Scrivocodice <info@scrivocodice.it>
2024-12-20 10:08:05 +0100

    Aggiunge codice dell'articolo del 20250129

---
+++ 20250129/my-themed-blog/src/pages/chi-siamo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const ChiSiamo = () => (
+ <div>
+   <h1>Chi Siamo</h1>
+   <p>Benvenuti sul nostro blog! Qui troverete informazioni su di noi e sul nostro progetto.</p>
+ </div>
+);
+
+export default ChiSiamo;
